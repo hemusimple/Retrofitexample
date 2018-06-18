7ced735a82e42577054e894336ff1150881cea9a
hemanth <hemanth9320@gmail.com>
2018-06-18 20:49:41 +0530

    added post example in sampltest.java interface

--- SmapleRetrofit/app/src/main/java/task/com/smapleretrofit/interfaces/SampleTest.java
+++ SmapleRetrofit/app/src/main/java/task/com/smapleretrofit/interfaces/SampleTest.java
@@ -3,8 +3,10 @@ package task.com.smapleretrofit.interfaces;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Url;
 
 /**
@@ -15,4 +17,8 @@ public interface SampleTest {
     @Headers({"Accept: application/json","Content-Type:application/json"})
     @GET()
     Call<ResponseBody> sampleget(@Url String fileUrl);
+
+    @FormUrlEncoded
+    @POST()
+    Call<ResponseBody> samplepost(@Url String fileurl);
 }
